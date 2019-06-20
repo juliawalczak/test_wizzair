52216282bae861975f897d1531bc8fd29f5f65e0
juliawalczak <juliawalczak@users.noreply.github.com>
2019-06-20 18:15:54 +0200

    added file: test_data.py

---
+++ test_data.py
@@ -0,0 +1,7 @@
+﻿valid_name = 'Alicja'
+valid_surname = 'Kot'
+valid_gender = 'female'
+valid_telephone = 985275975
+invalid_email = 'alakoto2.pl'
+valid_password = 'tewjro9325'
+valid_country = 'Malta'
